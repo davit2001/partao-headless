0d1ccc81c66067a34780a3962b1efb409c1404fc
Davit Hakobyan <david.hakobyan2001@gmail.com>
2025-03-28 18:58:18 +0400

    Change build widget

--- src/scripts/build-widgets.js
+++ src/scripts/build-widgets.js
@@ -1,13 +1,16 @@
-const { build } = require('esbuild');
+import { build } from 'esbuild';
 
 build({
   entryPoints: ['src/widgets/entry.tsx'],
   outfile: 'public/widgets/ui.js',
   bundle: true,
   format: 'iife',
-  globalName: 'MagentoUI',
-  minify: true,
-  sourcemap: true,
-  target: ['esnext'],
   platform: 'browser',
-}).catch(() => process.exit(1));
+  target: ['esnext'],
+  sourcemap: true,
+  minify: true,
+  define: {
+    'process.env.NODE_ENV': '"production"',
+    'process.env': '{}',
+  },
+})
