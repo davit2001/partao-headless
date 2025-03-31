71da293ebacbfb1cec43efa62d9d4382025c041b
Davit Hakobyan <david.hakobyan2001@gmail.com>
2025-03-31 13:11:18 +0400

    Change header and footer for testing

--- src/widgets/entry.tsx
+++ src/widgets/entry.tsx
@@ -1,18 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 
 (window as Window & typeof globalThis & {
   mountReactHeader: (el: HTMLElement) => void;
 }).mountReactHeader = (el) => {
   const root = createRoot(el);
-  root.render(<Header />);
+  root.render(<p>Header</p>);
 };
 
 (window as Window & typeof globalThis & {
   mountReactHeader: (el: HTMLElement) => void;
 }).mountReactHeader = (el) => {
   const root = createRoot(el);
-  root.render(<Footer />);
+  root.render(<p>Footer</p>);
 };
