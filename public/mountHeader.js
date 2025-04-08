2d66dbd5818089f647c4cfeb67c7dea8a61613db
Davit Hakobyan <david.hakobyan2001@gmail.com>
2025-04-08 18:19:35 +0400

    Add mountHeader function to render Header component

---
+++ public/mountHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Header from "../src/components/Header.js"
+
+export function mountHeader(containerId) {
+  const container = document.getElementById(containerId);
+  if (container) {
+    const root = createRoot(container);
+    root.render(<Header />);
+  }
+}
